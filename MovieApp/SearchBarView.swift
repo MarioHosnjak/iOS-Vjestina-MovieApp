8f373317ee402cfe41c2d90dcfb085e660b748b8
Mario Hosnjak <mariohosnjak@Marios-MacBook-Pro.local>
2022-04-11 12:40:42 +0200

    dz2_1.1

---
+++ MovieApp/SearchBarView.swift
@@ -0,0 +1,135 @@
+//
+//  SearchBarView.swift
+//  MovieApp
+//
+//  Created by Mario Hosnjak on 10.04.2022..
+//
+
+import SwiftUI
+import Foundation
+import UIKit
+import PureLayout
+
+class SearchBarView: UIView {
+    
+    var searchBarTextField = UITextField()
+    var magGlassButton = UIButton()
+    var cancelButton = UIButton()
+    var xButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+        buildViews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func buildViews() {
+        
+        addSubview(searchBarTextField)
+        addSubview(magGlassButton)
+        addSubview(cancelButton)
+        addSubview(xButton)
+        
+        self.backgroundColor = UIColor(red: 0.917, green: 0.917, blue: 0.921, alpha: 1)
+        self.layer.cornerRadius = 10
+        
+        searchBarTextField.attributedPlaceholder = NSAttributedString("Search")
+        searchBarTextField.addTarget(self, action: #selector(textFieldSelected2), for: UIControl.Event.editingDidBegin)
+        
+        magGlassButton.autoSetDimension(.width, toSize: 20.31)
+        magGlassButton.setBackgroundImage(UIImage(named: "magnifyingglass"), for: .normal)
+        magGlassButton.setBackgroundImage(UIImage(named: "magnifyingglass"), for: .selected)
+        magGlassButton.addTarget(self, action: #selector(didCancelButtonClick2), for: .touchUpInside)
+        
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.setTitleColor(.black, for: .normal)
+//        cancelButton.backgroundColor = .yellow
+        cancelButton.isHidden = true
+        cancelButton.addTarget(self, action: #selector(didCancelButtonClick2), for: .touchUpInside)
+        
+        xButton.setBackgroundImage(UIImage(named: "xmark"), for: .normal)
+        xButton.setBackgroundImage(UIImage(named: "xmark"), for: .selected)
+        xButton.autoSetDimension(.width, toSize: 10)
+        xButton.autoSetDimension(.height, toSize: 10)
+        xButton.isHidden = true
+        xButton.addTarget(self, action: #selector(didXButtonnClick2), for: .touchUpInside)
+        
+    }
+    
+    private func addConstraints(){
+        
+        searchBarTextField.autoPinEdge(.leading, to: .trailing, of: magGlassButton, withOffset: 15)
+        searchBarTextField.autoPinEdge(.top, to: .top, of: self, withOffset: 5)
+        searchBarTextField.autoPinEdge(.bottom, to: .bottom, of: self, withOffset: -5)
+        searchBarTextField.autoPinEdge(.trailing, to: .trailing, of: self, withOffset: -50)
+        
+        magGlassButton.autoPinEdge(.leading, to: .leading, of: self, withOffset: 15)
+        magGlassButton.autoPinEdge(.top, to: .top, of: self, withOffset: 15)
+        magGlassButton.autoPinEdge(.bottom, to: .bottom, of: self, withOffset: -15)
+        
+        cancelButton.autoPinEdge(.top, to: .top, of: self, withOffset: 5)
+        cancelButton.autoPinEdge(.leading, to: .trailing, of: self, withOffset: 10)
+        cancelButton.autoPinEdge(.bottom, to: .bottom, of: self, withOffset: -5)
+        
+//        cancelButton.autoSetDimension(.width, toSize: 80)
+        
+        xButton.autoPinEdge(.trailing, to: .trailing, of: self, withOffset: -15)
+        xButton.autoPinEdge(.top, to: .top, of: self, withOffset: 20)
+        
+    }
+
+    @objc func textFieldSelected2(){
+        print("CLICKED!")
+        for constraint in self.constraints {
+            if constraint.firstAttribute == .width {
+                constraint.constant = self.superview!.frame.width * 0.7
+            }
+        }
+        cancelButton.isEnabled = true
+        cancelButton.isUserInteractionEnabled = true
+        cancelButton.isHidden = false
+        xButton.isHidden = false
+        
+    }
+    
+
+    @objc public func didCancelButtonClick2(){
+        print("cancel button clicked")
+        self.endEditing(true)
+        cancelButton.isHidden = true
+        xButton.isHidden = true
+        searchBarTextField.text = ""
+        for constraint in self.constraints {
+            if constraint.firstAttribute == .width {
+                constraint.constant = self.superview!.frame.width * 0.9
+                }
+        }
+    }
+    
+
+    @objc func didSearchButtonClick2(){
+        print("SEARCH!")
+        print("cancel button clicked")
+        self.endEditing(true)
+        cancelButton.isHidden = true
+        xButton.isHidden = true
+        searchBarTextField.text = ""
+        for constraint in self.constraints {
+            if constraint.firstAttribute == .width {
+                constraint.constant = self.superview!.frame.width * 0.9
+                }
+        }
+    }
+    
+
+    @objc func didXButtonnClick2(){
+        searchBarTextField.text = ""
+    }
+
+    
+}
---
+++ MovieApp/myCollectionView.swift
@@ -0,0 +1,114 @@
+//
+//  myCollectionView.swift
+//  MovieApp
+//
+//  Created by Mario Hosnjak on 11.04.2022..
+//
+
+import SwiftUI
+import Foundation
+import UIKit
+import PureLayout
+import MovieAppData
+
+class myCollectionView: UIView {
+    
+    var tableView: UITableView!
+    let cellIdentifier = "cellId"
+    
+    let cellSpacingHeight: CGFloat = 50
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+        buildViews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func buildViews() {
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height))
+        self.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier) // 1.
+        tableView.dataSource = self // 2.
+        tableView.backgroundColor = .white
+    }
+    
+    private func addConstraints(){
+        
+    }
+    
+}
+extension myCollectionView: UITableViewDataSource { // 3.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return Movies.all().count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(
+//                withIdentifier: cellIdentifier,
+//                    for: indexPath) // 4.
+        let cell = UITableViewCell()
+        
+        tableView.rowHeight = 170
+        tableView.separatorStyle = .none
+        
+//        // Apply rounded corners
+//        contentView.layer.cornerRadius = 5.0
+//        contentView.layer.masksToBounds = true
+        // Set masks to bounds to false to avoid the shadow
+        // from being clipped to the corner radius
+        cell.layer.cornerRadius = 5.0
+        cell.layer.masksToBounds = false
+        
+        // How blurred the shadow is
+        cell.layer.shadowRadius = 8.0
+        // The color of the drop shadow
+        cell.layer.shadowColor = UIColor.black.cgColor
+        // How transparent the drop shadow is
+        cell.layer.shadowOpacity = 0.15
+        // How far the shadow is offset from the UICollectionViewCell’s frame
+        cell.layer.shadowOffset = CGSize(width: 0, height: 5)
+
+        
+        let row = indexPath.section
+        
+        let urlString = Movies.all()[row].imageUrl
+        let url = URL(string: urlString)
+        let image: UIImageView
+        image = UIImageView()
+        image.load(url: url!)
+        image.frame = CGRect(x: 10, y: 10, width: 100, height: 150)
+        
+        let title = "\(Movies.all()[row].title) (\(Movies.all()[row].year))"
+        let titleLabel = UILabel(frame: CGRect(x: 125, y: 10, width: self.frame.width - 100 - 20 - 20.0, height: 70))
+        titleLabel.text = title
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        titleLabel.numberOfLines = 2
+        
+        let desc = Movies.all()[row].description
+        let descLabel = UILabel(frame: CGRect(x: 125, y: 60, width: self.frame.width - 100 - 20 - 20.0, height: 100))
+        descLabel.text = desc
+        descLabel.font = UIFont.systemFont(ofSize: 15)
+        descLabel.numberOfLines = 5
+        
+        cell.addSubview(image)
+        cell.addSubview(titleLabel)
+        cell.addSubview(descLabel)
+        
+        
+//        var cellConfig = UIListContentConfiguration() // 5.
+//
+//
+//        cell.contentConfiguration = cellConfig
+        return cell
+    }
+}
